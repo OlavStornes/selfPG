d26026d0525c5a63eb039a778d50aeaac4f54f72
OlavStornes <ost028@post.uit.no>
2017-09-25 17:24:32 +0200

    Renamed for clarity

---
+++ test_units.py
@@ -0,0 +1,38 @@
+import unittest
+from units import *
+
+
+class Herotest(unittest.TestCase):
+
+    def setUp(self):
+        self.guy1 = Unit("Testhero", 10, 4, 1)
+        self.guy2 = Unit("Testbaddie", 20, 4, 1)
+
+
+    def test_get_experience(self):
+        self.assertEqual(self.guy1.xp, 0)
+        self.guy1.get_experience(5)
+        self.assertEqual(self.guy1.xp, 5)
+
+    def test_level_up(self):
+        self.assertEqual(self.guy1.lvl, 1)
+        self.guy1.level_up()
+        self.assertEqual(self.guy1.lvl, 2)
+
+    def test_rest(self):
+        self.guy1.hp = 1
+        self.guy1.rest()
+        self.assertEqual(self.guy1.hp, self.guy1.maxhp)
+
+    def test_attack(self):
+        self.guy1.attack(self.guy2)
+        self.assertLess(self.guy2.hp, self.guy2.maxhp)
+
+    def test_search_target(self):
+        badparty = Party()
+        badparty.join_party(self.guy2)
+        self.guy1.search_target(badparty)
+        self.assertEqual(self.guy1.target, self.guy2)
+
+if __name__ == '__main__':
+    unittest.main()
