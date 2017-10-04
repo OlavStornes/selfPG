5b6867a7bc077fdd36967d52a25bd1b3a85b77f0
OlavStornes <ost028@post.uit.no>
2017-10-04 18:43:56 +0200

    Changed partyframe to a listbox

--- gui.py
+++ gui.py
@@ -12,12 +12,15 @@ class Party_gui(tk.Frame):
         self.pack()
         self.master.title(PARTY_TITLE)
 
+        self.debug_tick = tk.IntVar()
+
         self.init_widgets()
         self.init_menu()
         self.createText_log()
         self.party = m.Party(self.T_log)
         self.test_createparty()
         self.create_partyframe()
+        self.create_baddieframe()
 
         self.baddieframe = None
 
@@ -28,77 +31,70 @@ class Party_gui(tk.Frame):
     def test_createparty(self):
         """DEBUG: Creates a party for testing purposes"""
 
-        self.party.join_party(m.Unit("Stronk1", self.party, 20, 6, 1))
-        self.party.join_party(m.Unit("Stronk2", self.party, 20, 6, 1))
+        self.party.join_party(m.Unit("Stronk1", 20, 6, 1))
+        self.party.join_party(m.Unit("Stronk2", 20, 6, 1))
 
         
     def print_log(self, string):
         self.T_log.insert(tk.END, string)
 
+    def update_partylist(self, listbox, partylist, party_string):
+        listbox.config(height=len(partylist),
+                        width=50)
+        
+        party_string.set(partylist)
+
     def update_statusbar(self):
         self.statusbar = tk.Label(self, text=self.party.activity, bg="darkgreen").grid(row=0, column=1)
-
-    def update_partyframe(self, partylist, variable_array):
-        """Updates party in GUI"""
-
-        #Clean up partyframe
-        for i in range(len(variable_array)):
-            variable_array[i].set("")
-
-        for i, npc in enumerate(partylist):
-            variable_array[i].set(npc)
-
 
     def create_partyframe(self):
         """Create a frame where partymembers go"""
         #TODO: Try to create a child window for specific heroes
         self.pframe_var = tk.StringVar()
-        self.partyframe = tk.LabelFrame(self, bg="green", text="Partymembers:")
+        self.partyframe = tk.Listbox(self, 
+                        bg="green",
+                        listvariable=self.pframe_var,
+                        activestyle="dotbox",
+                        exportselection=0
+                        )
 
-        self.pframe_var_array = []
+        self.pframe_var.set (self.party.members)
 
+        self.partyframe.grid(row=0 ,column=0, rowspan=5)
 
-        for i in range(len(self.party.members)):
-            self.pframe_var_array.append(tk.StringVar())
-            tk.Label(self.partyframe, textvariable=self.pframe_var_array[i]).pack()
-
-
-        self.partyframe.grid(row=0 ,column=0, rowspan=10)
 
 
     def create_baddieframe(self):
 
         self.bframe_var = tk.StringVar()
-        self.baddieframe = tk.LabelFrame(self, bg="red", text="Enemies:")
+        self.baddielist = tk.Listbox(self, 
+                                    bg="red", 
+                                    listvariable=self.bframe_var)
 
-        self.bframe_var_array = []
-
-
-        for i in range(len(self.party.cur_fight.baddieparty.members)):
-            self.bframe_var_array.append(tk.StringVar())
-            tk.Label(self.baddieframe, textvariable=self.bframe_var_array[i]).pack()
+        #self.pframe_var.set (self.party.cur_fight.baddieparty.members)
 
 
-        self.baddieframe.grid(row=10 ,column=0, rowspan=10)
+        self.baddielist.grid(row=10 ,column=0, rowspan=5)
 
 
     def test_tick(self):
         """Main-loop with ticks"""
         self.party.tick()
         self.update_gui()
-        self.after(GUI_UPDATE_RATE, self.test_tick)
+
+        
+        if self.debug_tick.get():
+            self.after(GUI_UPDATE_RATE, self.test_tick)
+
 
 
     def update_gui(self):
         """Update gui with all elements"""
         self.update_statusbar()
-        self.update_partyframe(self.party.members, self.pframe_var_array)
+        self.update_partylist(self.partyframe, self.party.members, self.pframe_var)
 
         if self.party.cur_fight:
-            if self.party.cur_fight.turn == 0:
-                self.create_baddieframe()
-            else:
-                self.update_partyframe(self.party.cur_fight.baddieparty.members, self.bframe_var_array)
+            self.update_partylist(self.baddielist, self.party.cur_fight.baddieparty.members, self.bframe_var)
             
 
 
@@ -129,14 +125,15 @@ class Party_gui(tk.Frame):
         tk.Button(buttonframe, text="QUIT", fg="red", command=self.quit).pack()
         tk.Button(buttonframe, text="START", fg="green", command=self.test_tick).pack()
         tk.Button(buttonframe, text="testfight", command=self.test_fight).pack()
-        tk.Button(buttonframe, text="testprint", command=self.insert_txt).pack()
+        tk.Button(buttonframe, text="testtick", command=self.test_tick).pack()
+        tk.Checkbutton(buttonframe, text="DB: AUTOTICK", variable=self.debug_tick).pack()
 
     def init_menu(self):
         menubar = tk.Menu(self)
 
         filemenu = tk.Menu(menubar, tearoff=0)
         #filemenu.add_command(label="Open", command=lambda: print("This feature isnt implemented yet"))
-        #filemenu.add_command(label="Save", command=lambda: print("hello"))
+        filemenu.add_command(label="DEBUG:recruit", command=self.test_createparty)
         filemenu.add_command(label="Start", command=self.test_tick)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.quit)
